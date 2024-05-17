d7dc9fbb1422545a22b22a230dfc88d30989932c
Max Frerichs <max.frerichs@lfda.de>
2024-05-17 11:58:15 +0200

    [BUGFIX] fix 403 error

--- Classes/Middleware/PrometheusMiddleware.php
+++ Classes/Middleware/PrometheusMiddleware.php
@@ -24,7 +24,7 @@ class PrometheusMiddleware implements MiddlewareInterface
         private readonly ExtensionConfiguration $extensionConfiguration,
     ) {
     }
-    
+
     public function process(
         ServerRequestInterface $request,
         RequestHandlerInterface $handler
@@ -32,27 +32,27 @@ class PrometheusMiddleware implements MiddlewareInterface
         $requestPort = $request->getAttributes()['normalizedParams']->getRequestPort();
         $metricsPath = $this->extensionConfiguration->get('typo3_prometheus', 'metricsPath');
         $metricsPort = $this->extensionConfiguration->get('typo3_prometheus', 'metricsPort');
-        
-        // fuck off everyone that tries to access metrics from outside 
-        if ($metricsPort && $requestPort != $metricsPort) {
-            return $this->responseFactory->createResponse(403);
-        }
 
         if ($request->getRequestTarget() == $metricsPath) {
             $statusProviders = $this->statusRegistry->getProviders();
             $collectorRegistry = new CollectorRegistry(new InMemory());
+            // fuck off everyone that tries to access metrics from outside 
+            if ($metricsPort && $requestPort != $metricsPort) {
+                return $this->responseFactory->createResponse(403);
+            }
             /**
              * Some status reports use LanguageService defined in this global variable to translate labels, but it's not initiated at the time
              * middleware is executed. While it's discouraged to instantiate on your own, I think it's okay in this case 
              * because not much other TYPO3-related stuff is to be executed afterwards
              */
-            $GLOBALS['LANG'] = GeneralUtility::makeInstance(LanguageServiceFactory::class)->createFromUserPreferences($GLOBALS['BE_USER']);;
+            $GLOBALS['LANG'] = GeneralUtility::makeInstance(LanguageServiceFactory::class)->createFromUserPreferences($GLOBALS['BE_USER']);
+            ;
             foreach ($statusProviders as $statusProviderItem) {
                 $status = $statusProviderItem->getStatus();
-                foreach ($status as $index=>$statusItem) {
-                    $metricName = strtolower(preg_replace("/[ .\/,-]/", "", $statusItem->getTitle())).(string)$index;
+                foreach ($status as $index => $statusItem) {
+                    $metricName = strtolower(preg_replace("/[ .\/,-]/", "", $statusItem->getTitle())) . (string) $index;
                     $gauge = $collectorRegistry->registerGauge("typo3", $metricName, "severity", ['message']);
-                    $gauge->set((float)$statusItem->getSeverity()->value, [strip_tags($statusItem->getMessage())]);
+                    $gauge->set((float) $statusItem->getSeverity()->value, [strip_tags($statusItem->getMessage())]);
                 }
             }
             $renderer = new RenderTextFormat();
